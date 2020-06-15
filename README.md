3fed459751b110fb43d82ffddd0d5e135da0424f
Matt Thompson <matt@aevv.net>
2020-06-15 13:58:04 +0100

    Test pwsh lists installed versions

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # wsl-setup
+
+Installs packages needed for dotnet core development on debian 10 (for use with WSL on windows)
+
+## Running
+
+In WSL, execute `./setup-debian.sh`
+
+Execute `./test.ps1` to verify things are installed correctly
--- test.ps1
+++ test.ps1
@@ -1,3 +1,9 @@
 #!/usr/bin/pwsh
 
-Write-Host "Hello world"
+$powershellVersion = & pwsh --version
+$dotnetVersion = & dotnet --version
+$funcVersion = & func --version
+
+Write-Host "$powershellVersion"
+Write-Host "dotnet $dotnetVersion"
+Write-Host "functions core tools $funcVersion"
